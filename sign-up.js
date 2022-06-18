f3327c6e4c30cd8777648744695518632fe168a7
denad00 <106638143+denad00@users.noreply.github.com>
2022-06-18 14:11:05 -0700

    Update sign-up.js

--- sign-up.js
+++ sign-up.js
@@ -1,24 +1,6 @@
 
 const db = firebase.firestore();
 
-// class User {
-//     constructor (name, phone, email) {
-//        this.fullName = name;
-//        this.phone = phone;
-//        this.email = email;
-//      }
-// }
-
-// let userArray = [];
-
-
-// signUp.addEventListener ("click", () => {
-//     let user = new User (fullName.value, phone.value, email.value)
-//     userArray.push(user);
-//     console.log(userArray)
-//     const outcome = db.collection("user").add(user)
-// })
-
 const user = [];
 
 signUp.addEventListener ("click", () => {
