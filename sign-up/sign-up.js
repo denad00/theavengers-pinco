22c5bc675eb1d18b95886abab60e0e919d317271
denad00 <denad00@mylangara.ca>
2022-06-26 12:54:21 -0700

    updates to sign up page and creation of login page

--- sign-up/sign-up.js
+++ sign-up/sign-up.js
@@ -3,21 +3,29 @@ const db = firebase.firestore();
 
 const user = [];
 
-signUp.addEventListener ("click", () => {
-  const user = {
-    name: fullName.value,
-    phone: phone.value,
-    email: email.value,
-    emergencyContact: "",
-    userID: Math.floor(Math.random() * Math.floor(Math.random() * Date.now()))
+document.getElementById('signUp').addEventListener ("click", () => {
+
+  if (password.value !== confirmPassword.value){
+    document.getElementById('errorMessage').innerHTML = "Passwords do not match, please try again";
+
+  } else {
     
+    const user = {
+      name: fullName.value,
+      phone: phone.value,
+      email: email.value,
+      password: password.value,
+      userID: Math.floor(Math.random() * Math.floor(Math.random() * Date.now()))
+      
+    }
+
+    console.log(user);
+    const outcome = db.collection("user").add(user);
+
+    Notification.requestPermission().then((result) => {
+      if (result === 'granted') {
+          randomNotification();
+       }
+      });
   }
-  console.log(user);
-  const outcome = db.collection("user").add(user);
-  
- Notification.requestPermission().then((result) => {
- if (result === 'granted') {
-     randomNotification();
-  }
- });
 });
