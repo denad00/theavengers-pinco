da58b12edf4fbb9b25dff55708b26190fb002365
denad00 <denad00@mylangara.ca>
2022-06-27 11:54:17 -0700

    sos button issue

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js")
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js")
+
+
+const firebaseConfig = { 
+    apiKey: "AIzaSyBybnwAFnoIbIbxbOQMLEHOaiO796YviRY",
+    authDomain: "langara-wmdd4885-avengers.firebaseapp.com",
+    databaseURL: "https://langara-wmdd4885-avengers-default-rtdb.firebaseio.com",
+    projectId: "langara-wmdd4885-avengers",
+    storageBucket: "langara-wmdd4885-avengers.appspot.com",
+    messagingSenderId: "1078303270426",
+    appId: "1:1078303270426:web:d7a2c3b43fd70e113053a3",
+    measurementId: "G-F4KJKNQE5T"
+};
+
+firebase.initializeApp(firebaseConfig); 
--- sos-button/sos-button.js
+++ sos-button/sos-button.js
@@ -1,12 +1,44 @@
 
 const db = firebase.firestore();
 
+const messaging = firebase.messaging();
 
-    sosButton.addEventListener ('click', () =>{
-        // new Notification('Danika has indicated she is in danger, please reach out to her');
+const button = document.getElementById('notifications');
+
+document.getElementById('subscribe').addEventListener('click', () => {
+    function subscribeUser(){
+        Notification.requestPermission().then((result) =>{
+            console.log(permission)
+            if(permission === "granted"){
+                messaging.getToken({vapidKey:"BHVN1l6afas-f6G-9UYH6-_ozo7UL9uXDEek2smA0yxRvB8WIADVK_G6KFfzPpomFsmC823q4PpF_-THvMZr5Qo"}.then(currentToken => {
+                    console.log(currentToken)
+                }));
+            }
+        })
+    }
+})
+
+    document.getElementById('sosButton').addEventListener ('click', () =>{
         const sos = {
             sosEvent: true,
+
         }
         const outcome = db.collection("userStatus").add(sos)
         console.log (sos);
-    })
+    });
+
+    // exports.sosTrigger = functions.firestore.document('users/{userId}').onUpdate(async (snap, context) => {
+    //     const newValues = snap.after.data();
+
+    //     const previousValues = snap.before.data();
+
+    //     if (newValues.sosEvent === true) {
+    //         const snapshot = await db.collection('userStatus').where('sosEvent', '===', 'true').get();
+
+    //         sosNotification();
+    //     }
+    // });
+
+    // function sosNotification(){
+    //     new Notification(`${db.collection('user').name} has indicated they are in danger`);
+    // }
