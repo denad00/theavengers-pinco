bb49739e079adfb15d897a924eddf7da17b4b5f1
Harshit <harshitpunn@gmail.com>
2022-07-09 16:47:28 -0700

    Refactored-code: ADDED code as per relevant structure and refactored authentication code

---
+++ js/scripts.js
@@ -0,0 +1,84 @@
+(function () {
+
+  const firebaseApp = firebase.initializeApp({ 
+    apiKey: "AIzaSyBybnwAFnoIbIbxbOQMLEHOaiO796YviRY",
+    projectId: "langara-wmdd4885-avengers"
+  });
+
+  console.log(firebaseApp)
+
+  const db = firebaseApp.firestore();
+  const auth = firebaseApp.auth();
+
+  const register = () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    auth.createUserWithEmailAndPassword(email, password).then((res) => {
+      console.log(res);
+    }).catch((error) => {
+      console.log(error.message);
+    });
+  };
+
+  const login = () => {
+    if (firebaseApp.auth().currentUser) {
+      firebaseApp.auth().signOut();
+    } else {
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+
+      auth.signInWithEmailAndPassword(email, password).then((res) => {
+        console.log(res);
+      }).catch((error) => {
+        console.log(error.code);
+        console.log(error.message);
+      });    
+    }
+  }
+
+  const saveUser = () => {
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    db.collection('user').add({
+      email: email,
+      password: password
+    })
+    .then((docRef) => {
+      console.log("Document ID", docRef.id)
+    }).catch((error) => {
+      console.error("Error",error);
+    })
+   
+  };
+
+  const readData = () => {
+    db.collection('users').get().then((data) => {
+      console.log(data.docs.map((item) => {
+        return {...item.data(), id: item.id };
+      }));
+    })
+  }
+
+  const initializeApp = () => {
+    firebaseApp.auth().onAuthStateChanged(function(user) {
+      if (user) {
+
+      }
+      else{
+        
+      }
+    document.getElementById('sign-in').addEventListener('click', login, false);
+    document.getElementById('sign-up').addEventListener('click', register, false);
+  })};
+
+  window.onload = function() {
+    initializeApp();
+  };
+
+  
+})();
+
+
