b9d35ca280483cba6174c5f947b56567309f20f9
Iris Chen <jaminitaa0322@gmail.com>
2022-07-11 15:58:12 -0700

    allow contact obsever to click and open push notification. change logo.

--- contact_observer/firebase-messaging-sw.js
+++ contact_observer/firebase-messaging-sw.js
@@ -45,12 +45,34 @@ console.log(self.registration);
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  const notificationTitle = 'Background Message Title';
+  const notificationTitle = 'SOS message';
   const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
+    body: payload.data.text,
+    icon: '/icon-192x192.png'
   };
 
   self.registration.showNotification(notificationTitle,
     notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  // TODO: add userID to URL
+    let url = `https://PincoContactObserverPage.irischen11.repl.co${userId}`;
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
 });
