c73afe67845502db8fa4fb4b7e46a0e2e8712a44
Harshit <harshitpunn@gmail.com>
2022-07-18 16:31:22 -0700

    refactored-code: FIXING login flow

---
+++ js/auth.js
@@ -0,0 +1,86 @@
+(function () {
+  const firebaseApp = firebase.initializeApp({ 
+    apiKey: "AIzaSyBybnwAFnoIbIbxbOQMLEHOaiO796YviRY",
+    projectId: "langara-wmdd4885-avengers"
+  });
+
+  const db = firebaseApp.firestore();
+  const auth = firebaseApp.auth();
+  const register = () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    auth.createUserWithEmailAndPassword(email, password).then((res) => {
+      console.log(res);
+    }).catch((error) => {
+      console.log(error.message);
+    });
+  };
+
+  const login = () => {
+    if (firebaseApp.auth().currentUser) {
+      firebaseApp.auth().signOut();
+    } else {
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+
+      auth.signInWithEmailAndPassword(email, password).then((res) => {
+        window.location.replace('index.html');
+        console.log(res);
+      }).catch((error) => {
+        console.log(error.code);
+        console.log(error.message);
+      });    
+    }
+  }
+
+  const saveUser = () => {
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    db.collection('user').add({
+      email: email,
+      password: password
+    })
+    .then((docRef) => {
+      console.log("Document ID", docRef.id)
+    }).catch((error) => {
+      console.error("Error",error);
+    })
+   
+  };
+
+  const readData = () => {
+    db.collection('users').get().then((data) => {
+      console.log(data.docs.map((item) => {
+        return {...item.data(), id: item.id };
+      }));
+    })
+  }
+
+  const initializeApp = () => {
+    firebaseApp.auth().onAuthStateChanged(function(user) {
+      if (user) {
+
+      }
+      else{
+        window.location.replace('/index.html');
+      }
+      
+      if(document.getElementById('sign-in')) {
+        document.getElementById('sign-in').addEventListener('click', login, false);
+      }
+
+      if(document.getElementById('sign-up')) {
+        document.getElementById('sign-up').addEventListener('click', register, false);
+      }
+    
+  })};
+
+  window.onload = function() {
+    initializeApp();
+  };
+
+  
+})();
---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+(function () {
+
+  const firebaseApp = firebase.initializeApp({ 
+    apiKey: "AIzaSyBybnwAFnoIbIbxbOQMLEHOaiO796YviRY",
+    projectId: "langara-wmdd4885-avengers"
+  });
+
+  const db = firebaseApp.firestore();
+  const auth = firebaseApp.auth();
+
+})();
