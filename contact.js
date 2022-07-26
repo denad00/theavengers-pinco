45c72ae5339df6506fad8d159755daba5b82630d
Iris Chen <jaminitaa0322@gmail.com>
2022-07-26 00:07:34 -0700

    Add updateUI and deletebtn to contact page

--- contact.js
+++ contact.js
@@ -3,12 +3,13 @@
 const contactCollection = db.collection("contact")
 
 const allFlows = document.querySelectorAll('div.contactFlow')
+const otherContactsOutput = document.getElementById("otherContactsOutput");
+const emergencyContactsOutput = document.getElementById("emergencyContactsOutput");
 console.log(allFlows)
 allFlows[0].style.display = 'block';
 allFlows[1].style.display = 'none';
 if (allFlows[0].style.display = 'block') {
-  getUserContactsList("iristest0001", updateContactsListHTML);
-  getUserEmergencyContactsList("iristest0001", updateEmergencyContactsListHTML);
+  updateContactsListHTML("iristest0001");
 }  //show current contact list
 
 const createBtn = document.getElementById("createBtn");
@@ -38,8 +39,7 @@ contactSubmit.addEventListener ("click", function(event) {
   const outcome = contactCollection.add(contact)
 .then((docRef) => {
     console.log("Document written with ID: ", docRef.id);
-    getUserContactsList("iristest0001", updateContactsListHTML);
-    getUserEmergencyContactsList("iristest0001", updateEmergencyContactsListHTML);
+    updateContactsListHTML("iristest0001");
 })
 .catch((error) => {
     console.error("Error adding document: ", error);
@@ -51,32 +51,19 @@ allFlows[1].style.display = 'none';
 
 //define function to generate contact list by firestore library method
 // userId: string, nextAction: function
-function getUserContactsList (userId, nextAction) { 
+function getUserContactsList (userId, isEmergency, nextAction) { 
     let contactsOfUser = [];
-    const q = contactCollection.where("userID", "==", userId).where("emergencyContact", "==", false).get()
+    const q = contactCollection.where("userID", "==", userId).where("emergencyContact", "==", isEmergency).get()
         .then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 // console.log(doc.data());
                 // console.log(doc.id);
-                contactsOfUser.push(doc.data()); //.data is a firestore method to get query object data 
+                let contactObj = doc.data();
+                contactObj["docID"] = doc.id
+                contactsOfUser.push(contactObj); //.data is a firestore method to get query object data 
             });
 
-            nextAction(contactsOfUser);
-        })
-        .catch((error) => {
-            console.log("Error getting documents: ", error);
-        });
-}
-
-function getUserEmergencyContactsList (userId, nextAction) { 
-    let contactsOfUser = [];
-    const q = contactCollection.where("userID", "==", userId).where("emergencyContact", "==", true).get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                contactsOfUser.push(doc.data()); //.data is a firestore method to get query object data 
-            });
-
-            nextAction(contactsOfUser);
+            nextAction(contactsOfUser, isEmergency);
         })
         .catch((error) => {
             console.log("Error getting documents: ", error);
@@ -86,23 +73,42 @@ function getUserEmergencyContactsList (userId, nextAction) {
 
 //define function to print out List of contact into html
 // contactsList: array
-function updateContactsListHTML (contactsList) {
+function generateContactsListHTML (contactsList, isEmergency) {
+  let contactListOutputElement = null;
+  if (isEmergency) {
+    contactListOutputElement = document.getElementById("emergencyContactsOutput");
+  } else {
+    contactListOutputElement = document.getElementById("otherContactsOutput");
+  }
+  contactListOutputElement.innerHTML = "";
+  console.log("[contact.js updateContactsListHTML] listLength: " + contactsList.length);
   for (let i = 0; i < contactsList.length; i++) {
+    
     let text = ""
     text += contactsList[i].name
+    text += "\n"
     text += contactsList[i].phone
-  otherContactsOutput.innerHTML += text
+    let line = document.createElement('li');
+    line.innerText += text
+    contactListOutputElement.appendChild(line);
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    line.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', (event) => {
+      console.log('delete index: ' + i + "\ncontactList[i]: " + contactsList[i].docID);
+      contactCollection.doc(contactsList[i].docID).delete().then(() => {    
+        updateContactsListHTML(contactsList[i].userID);
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+      
+		});
+    // otherContactsOutput.innerHTML += text
  }
 }
 
-function updateEmergencyContactsListHTML (contactsList) {
-  for (let i = 0; i < contactsList.length; i++) {
-    let text = ""
-    text += contactsList[i].name
-    text += contactsList[i].phone
-  emergencyContactsOutput.innerHTML += text
- }
+function updateContactsListHTML (userID) {
+  getUserContactsList(userID, false, generateContactsListHTML);
+  getUserContactsList(userID, true, generateContactsListHTML);
 }
-
-
-
