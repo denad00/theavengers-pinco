1a0128d73754068988744fe3597c50881f8f4b22
Iris Chen <jaminitaa0322@gmail.com>
2022-07-26 17:26:43 -0700

    update contact page UI, enable sharing sms popup and change contacts' type

--- contact.js
+++ contact.js
@@ -9,7 +9,7 @@ console.log(allFlows)
 allFlows[0].style.display = 'block';
 allFlows[1].style.display = 'none';
 if (allFlows[0].style.display = 'block') {
-  updateContactsListHTML("iristest0001");
+  updateContactsListHTML("ictestnotif01");
 }  //show current contact list
 
 const createBtn = document.getElementById("createBtn");
@@ -31,7 +31,7 @@ contactSubmit.addEventListener ("click", function(event) {
     name: contactName.value, //from html id
     phone: contactPhone.value, //from html id
     emergencyContact: cb.checked, //from html checkbox
-    userID: "iristest0001",  // get userID after log in, this is just a text id
+    userID: "ictestnotif01",  // get userID after log in, this is just a text id
     contactID: Math.floor(Math.random() * Math.floor(Math.random() * Date.now()))  //generate contact id
   }
   console.log(contact);
@@ -39,15 +39,18 @@ contactSubmit.addEventListener ("click", function(event) {
   const outcome = contactCollection.add(contact)
 .then((docRef) => {
     console.log("Document written with ID: ", docRef.id);
-    updateContactsListHTML("iristest0001");
+    updateContactsListHTML("ictestnotif01");
 })
 .catch((error) => {
     console.error("Error adding document: ", error);
 });
 allFlows[0].style.display = 'block';
 allFlows[1].style.display = 'none';
+
+
   
 });
+
 
 //define function to generate contact list by firestore library method
 // userId: string, nextAction: function
@@ -94,18 +97,58 @@ function generateContactsListHTML (contactsList, isEmergency) {
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
     line.appendChild(deleteBtn);
+
+    contactListOutputElement.appendChild(line);
+    const starBtn = document.createElement("button");
+    starBtn.textContent = "star";
+    line.prepend(starBtn);
+
     deleteBtn.addEventListener('click', (event) => {
-      console.log('delete index: ' + i + "\ncontactList[i]: " + contactsList[i].docID);
+      // console.log('delete index: ' + i + "\ncontactList[i]: " + contactsList[i].docID);
       contactCollection.doc(contactsList[i].docID).delete().then(() => {    
         updateContactsListHTML(contactsList[i].userID);
         console.log("Document successfully deleted!");
-    }).catch((error) => {
-        console.error("Error removing document: ", error);
+        }).catch((error) => {
+            console.error("Error removing document: ", error);
+      });
+		});
+
+    starBtn.addEventListener('click', (event) => {
+      console.log({ emergencyContact: !contactsList[i].emergencyContact });
+      contactCollection.doc(contactsList[i].docID).update({ emergencyContact: !contactsList[i].emergencyContact }).then(() => {    
+        updateContactsListHTML(contactsList[i].userID);
+        console.log("Emergency contacts update successfully!");
+       }).catch((error) => {
+        console.error("Error updating emergency contacts: ", error);
     });
+
+    
       
 		});
+
+   
     // otherContactsOutput.innerHTML += text
  }
+}
+
+function changeEmergencyContactList() {
+
+  const q = contactCollection.where("emergencyContact", "==", false).get()
+  .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          console.log(doc.data());
+          // console.log(doc.id);
+          let emergencyStatus = emergencyContact;
+          // emergencyStatus = true;
+          console.log(emergencyStatus);
+      });
+  // let emergencyContact = 'emergencyContact';
+  // if (emergencyContact)
+  // emergencyContact = false;
+  // else {
+  //   emergencyContact = true
+  })
+    
 }
 
 function updateContactsListHTML (userID) {
