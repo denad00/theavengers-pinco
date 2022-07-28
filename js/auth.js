1a1b73f3dc9e6476ff2c9facf9f8f67ce26d167d
Harshit <harshitpunn@gmail.com>
2022-07-28 08:18:23 -0700

    signup-comp: ADDED code to fix registration form validation and styling

--- js/auth.js
+++ js/auth.js
@@ -14,18 +14,63 @@
   const auth = firebaseApp.auth();
 
   
-  const register = () => {
+  const register =  (e) => {
+    e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value
+    const error = document.getElementById('errorMessage');
+  
+    if (password !== confirmPassword) {
+      error.innerHTML = "Passwords do not match, please try again";
+      signupForm.reset();
+    
+    } else if (password.length < 6) {
+      error.innerHTML = "Passwords must be at least six characters"
+      signupForm.reset();
+    
+    } else {
 
-    auth.createUserWithEmailAndPassword(email, password).then((res) => {
-      console.log(res);
-    }).catch((error) => {
-      console.log(error.message);
-    });
-  };
+      auth.createUserWithEmailAndPassword(email, password).then(res => {
+        let user = auth.currentUser;
+        if(user != null) {
+          uid = user.uid;
+        }
+        const userName = document.getElementById('fullName').value
+        const userPhone = document.getElementById('phone').value;
+        const saveUser = {
+          name: userName,
+          phone: userPhone,
+          email: email,
+          password: password,
+          userID: uid,
+          emergencyContactID: "",
+        }
 
-  const login = () => {
+        const userStatus = {
+          email: email,
+          userID: uid,
+          time: new Date(),
+          longitutde: "",
+          latitude: "",
+          sosEvent: false,
+          tokenID: "",
+        } 
+
+        db.collection("user").add(saveUser)
+        db.collection("userStatus").add(userStatus)
+        swal('Your Account Created','Your account was created successfully, you can log in now.',
+        ).then((value) => {
+            setTimeout(function(){
+                window.location.replace("../index.html");
+            }, 1000)
+        });
+      });
+    };
+  }
+
+  const login = (e) => {
+    e.preventDefault();
     if (auth.currentUser) {
       auth.signOut();
     } else {
@@ -46,24 +91,45 @@
     }
   }
 
+  const validatePassword = (e) => {
+    let confirmPassword = e.target.value
+    let password = document.getElementById('password').value
+    const errorPasswordBox = document.getElementById("errorPassword")
+    const signUpButton = document.getElementById('signUp')
+
+    if(confirmPassword != password) {
+      errorPasswordBox.style.display = "block";
+      errorPasswordBox.innerText = 'Password do not match';
+      errorPasswordBox.style.padding = '1.5%';
+      signUpButton.disabled = true;
+    }
+    else {
+      errorPasswordBox.style.display = "none";
+      signUpButton.disabled = false;
+    }
+  }
+
   const initializeApp = () => {
     auth.onAuthStateChanged(function(user) {
       if (user) {
          window.location.replace('index.html');
       }
-      else{
-
-      }
     })
 
-    const signInFormButton = document.getElementById('signin');
+    const signInForm = document.getElementById('signin-form');
+    const signUpForm = document.getElementById('signup-form');
+    const confirmPassword = document.getElementById('confirmPassword');
 
-    if(signInFormButton) {
-      signInFormButton.addEventListener('click', login);
+    if(signInForm) {
+      signInForm.addEventListener('submit', login, false);
     }
 
-    if(document.getElementById('signup')) {
-      document.getElementById('signup').addEventListener('click', register, false);
+    if(signUpForm) {
+      signUpForm.addEventListener('submit', register, false);
+    }
+
+    if(confirmPassword) {
+      confirmPassword.addEventListener('keyup', validatePassword, false)
     }
 
 };
