5515b7f142b843d9a0ed8251870f3182c35e5bbb
Iris Chen <jaminitaa0322@gmail.com>
2022-08-04 16:48:25 -0700

    fix sos button to latest

--- script.js
+++ script.js
@@ -223,35 +223,6 @@ const updateCheckInInterval = setInterval(checkInHelper, 1000);
 /* ========================= CONTACT END =======================*/
 
 
-/* ========================= SOS ============================ */
-
-
-const sosEvent = document.getElementById('sosButton');
-let userStatus = db.collection("userStatus");
-let userStatusRef = null
-
-sosEvent.addEventListener ("click", function(event) {
-    userStatus.where("userID", "==", "ictestnotif01").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            userStatusRef = userStatus.doc(doc.id);
-            console.log(doc.data());
-            console.log (userStatusRef);
-        //   console.log(`${doc.id} => ${doc.data()}`);
-        })
-        userStatusRef.update(
-            // "sosEvent", "==", "true"
-            { sosEvent: true }
-            ).then(() => {
-            console.log("sosEvent updated");
-            }).catch((error) => {
-            console.log("Error updating document");
-            });
-      })
-
-
-    });
-    //   new Notification('Danika has indicated she is in danger, please reach out to her');
-
 /* =========================== SOS END ==================================== */
 
 
